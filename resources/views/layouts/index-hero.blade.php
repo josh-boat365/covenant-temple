caa4690a5ed9e205ead6a624d57024a81ffe2700
josh-boat365 <nyarko.boateng20@outlook.com>
2024-01-06 18:53:26 +0000

    base app v1.5

---
+++ resources/views/layouts/index-hero.blade.php
@@ -0,0 +1,106 @@
+<section class="intro intro-line context-dark">
+    <!--RD Navbar-->
+    @include('layouts.navbar1')
+    <div class="intro-line-table">
+        <div class="intro-line-row">
+            <div class="intro-line-col"></div>
+            <div class="intro-line-col intro-line-col-center"></div>
+            <div class="intro-line-col"></div>
+        </div>
+        <div class="intro-line-row intro-line-content">
+            <div class="intro-line-col"></div>
+            <div class="intro-line-col intro-line-col-center"></div>
+            <div class="intro-line-col"></div>
+        </div>
+        <div class="intro-line-row">
+            <div class="intro-line-col"></div>
+            <div class="intro-line-col intro-line-col-center intro-line-footer">
+                <div class="container" data-animate='{"class":"fadeIn"}'>
+                    <div class="row row-25">
+                        <div class="col-lg-9 col-xl-7 text-center text-lg-left">
+                            <!-- Blurb link-->
+                            <div class="blurb blurb-link-2">
+                                <div class="blurb-icon text-white linearicons-compass2"></div>
+                                <div class="blurb-content">
+                                    <h4 class="blurb-title"><a
+                                            href="https://www.google.com/maps/dir/5.572214,-0.1401392/triumphant+christian+center+pokuase/@5.6291029,-0.2962293,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0xfdf9f207170c2f5:0xb2548b738ccad5c6!2m2!1d-0.2866605!2d5.6844933?entry=ttu"
+
+                                            target="blank">GG1201831, Pokuase
+                                            Near Ampax Hotel, Off the Nsawam - Accra Road. </a></h4>
+                                    <div class="blurb-subtitle">Our doors are always opened, join us fellowships on
+                                        Mondays, Wednesdays, Fridays and Sundays</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-xl-5 text-center text-lg-right">
+                            <div class="group-15 group-lg-30">
+                                <a class="icon icon-md icon-link icon-link-gray mdi-facebook"
+                                    href="https://www.facebook.com/tccpokuaseofficial" target="blank"></a>
+                                <a class="icon icon-md icon-link icon-link-gray mdi-youtube-play"
+                                    href="https://www.youtube.com/@tccpokuase"></a>
+                                {{--  <a class="icon icon-md icon-link icon-link-gray mdi-linkedin" href="#"></a>
+                                <a class="icon icon-md icon-link icon-link-gray mdi-twitter" href="#"></a>  --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="intro-line-col"></div>
+        </div>
+    </div>
+    <!-- Swiper slider-->
+    <div class="swiper-container swiper-numbered" data-swiper>
+        <!-- Additional required wrapper-->
+        <div class="swiper-wrapper">
+            <!-- Slides-->
+            <div class="swiper-slide" style="background-image: url( images/background/bg-8-1920x930.jpg )">
+                <div class="intro-line-container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-7">
+                                <h3 class="font-italic font-secondary"> Welcome to Our Church</h3>
+                                <h1>Join the prayer</h1>
+                                <p class="big">TCC Logo Church is a Family of Faith that is committed to
+                                    Bible
+                                    teaching and joyful worship for children, youth and adults of all ages.</p>
+                                <a class="btn btn-white" href="#">See More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide" style="background-image: url( images/background/bg-9-1920x930.jpg )">
+                <div class="intro-line-container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-7">
+                                <h3 class="font-italic font-secondary"> Reconnect with the Creator</h3>
+                                <h1>Connect & Grow</h1>
+                                <p class="big">Our church is a perfect place for all local residents to come
+                                    in with their families and join for a prayer and a newfound peace of mind.
+                                </p><a class="btn btn-white" href="#">See More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide" style="background-image: url( images/background/bg-4-1920x930.jpg )">
+                <div class="intro-line-container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-7">
+                                <h3 class="font-italic font-secondary"> Embrace Your Faith</h3>
+                                <h1>Share the love</h1>
+                                <p class="big">At our church, we help people to find their way back to God.
+                                    We accomplish this by reaching people far from God.</p><a class="btn btn-white"
+                                    href="#">See More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Pagination-->
+        <div class="swiper-pagination"></div>
+    </div>
+</section>
