7b8c1e3b98bd20c1c5701787918800eb644aec80
josh-boat365 <nyarko.boateng20@outlook.com>
2024-01-06 12:48:08 +0000

    base app v.1.2

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('frontend.dist.index');
-});
+})->name('index');
+
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
